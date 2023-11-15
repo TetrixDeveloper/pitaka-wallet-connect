2825ad27ada46c6cff92579ae9cf7e174dfe0317
Raff <ralphacabado@live.com>
2023-11-15 12:58:58 +0800

    chore: updated theming options

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,16 @@ function App() {
           "14e5d957c6eb62d3ee8fc6239703ac2d537d7e3552154836ca0beef775f630bc",
         ]}
         explorerExcludedWalletIds="ALL"
+        termsOfServiceUrl="https://support.pitaka.io/terms-of-use"
+        privacyPolicyUrl="https://support.pitaka.io/privacy-policy"
+        themeMode="dark"
+        themeVariables={{
+          "--w3m-font-family": "Roboto, sans-serif",
+          "--w3m-accent-color": "#613CB0",
+          "--w3m-background-color": "#1A1A1A",
+          "--w3m-logo-image-url": "/pitaka.png",
+          "--w3m-overlay-backdrop-filter": "blur(10px)",
+        }}
       />
     </>
   );
